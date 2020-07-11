bee6c6d29ae7d604b22cf0999d756496f0acb421
kihoonkim <koreakihoon@gmail.com>
2020-07-11 12:34:07 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import * as firebase from 'firebase/app';
+import ReactIdSwiper from 'react-id-swiper';
+import 'swiper/swiper.scss';
+import shortid from 'shortid';
+
 import './App.css';
+import MainImage from './images/main.jpg';
+import Image1 from './images/1.jpg';
+import Image2 from './images/2.jpg';
+import Image3 from './images/3.jpg';
+import Image4 from './images/4.jpg';
+import Image5 from './images/5.jpg';
+import Image6 from './images/6.jpg';
+import Image7 from './images/7.jpg';
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  function writeUserData(name, comment) {
+    const id = shortid.generate();
+    firebase.database().ref('comments/' + id).set({
+      name,
+      comment,
+      createTime : new Date().getTime()
+    });
+  }
+
+  function readUserData() {
+    firebase.database().ref('comments')
+        .on('value', (snapshot) => {
+          const value = snapshot.val()
+          console.log(value)
+          if(!value) return;
+
+          setComments(Object.values(value).map(val => val));
+        });
+  }
+
+  function handleOnSubmit() {
+    writeUserData(name, comment);
+    setName('');
+    setComment('');
+  }
+
+  useEffect(() => {
+    readUserData();
+  }, []);
+
+  const images = [
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image5,
+    Image6,
+    Image7,
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>08 / 30</h1>
+      <div>최재근ㆍ김초록</div>
+      <div>결혼합니다</div>
+      <img src={MainImage} />
+      <div>2020년 8월 30일 일요일 오후 3시</div>
+      <div>삼성금융캠퍼스 지하 1층 비전홀</div>
+      <div>∨</div>
+      <div>INVITATION</div>
+      <div>{
+        `
+        두 사람이 사랑으로 만나\n
+        진실과 이해로써 하나를 이루려 합니다.\n
+        이 두사람을 지성으로 아끼고 돌봐주신\n
+        여러분들을 모시고 서약을 맺고자 하오니\n
+        바쁘신 가운데 두 사람의 앞날을\n
+        가까이에서 축복해 주시면 고맙겠습니다.\n
+        `
+      }</div>
+
+      <div>최용호 ㆍ 황해순 의 장남 재근</div>
+      <div>김석종 ㆍ 조정애 의 장녀 초록</div>
+
+      <div>2020년 8월 30일 일요일 오후 3시</div>
+      <div>삼성금융캠퍼스 지하 1층 비전홀</div>
+
+      <div>ㆍ</div>
+
+      <div>our photo</div>
+      <ReactIdSwiper>
+        {
+          images.map(({image}, index) => (
+              <img
+                  src={image}
+                  key={'image-' + index}
+                  width={100}
+                  height={100}
+              />
+          ))
+        }
+      </ReactIdSwiper>
+      <div>
+        <div>
+          name: <input value={name} onChange={(e) => setName(e.target.value)}/>
+        </div>
+        <div>
+          comment: <input value={comment} onChange={(e) => setComment(e.target.value)}/>
+        </div>
+        <button onClick={handleOnSubmit}>submit</button>
+      </div>
+      <div>
+        {
+          comments.map(({name, comment, createTime}) => {
+            return <div>{`${name}, ${comment}, ${createTime}`}</div>
+          })
+        }
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjlIXQyfCwimYf0y-mwm1qOZHER251ITc",
+  authDomain: "wedding-282208.firebaseapp.com",
+  databaseURL: "https://wedding-282208.firebaseio.com",
+  projectId: "wedding-282208",
+  storageBucket: "wedding-282208.appspot.com",
+  messagingSenderId: "452664017216",
+  appId: "1:452664017216:web:08e8b907b13cb2cbe87aa5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
